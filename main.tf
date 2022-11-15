9d14b38362ee9136782bafa23fffad07bff235e5
ansible <ansible@vm-ubuntu.com>
2022-11-15 12:14:35 +0530

    correcting spealing mistake

--- main.tf
+++ main.tf
@@ -7,22 +7,23 @@ variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable my_ip {}
+variable instance_type {}
+variable public_key_location {}
 
 
 
 resource "aws_vpc" "my-vpc" {
   cidr_block = var.vpc_cidr_block
-  tags = {
+  tags       = {
     Name = "${var.env_prefix}-vpc"
   }
 }
 
 resource "aws_subnet" "my-vpc-sunbet-1" {
-  vpc_id     = aws_vpc.my-vpc.id
-  cidr_block = var.subnet_cidr_block
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = var.subnet_cidr_block
   availability_zone = var.avail_zone
-
-  tags = {
+  tags              = {
     Name = "${var.env_prefix}-subnet"
   }
 }
@@ -30,7 +31,7 @@ resource "aws_subnet" "my-vpc-sunbet-1" {
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.my-vpc.id
 
-  tags = {
+  tags   = {
     Name = "${var.env_prefix}-igw"
   }
 }
@@ -83,4 +84,47 @@ resource "aws_security_group" "my-sg" {
   tags = {
     Name = "${var.env_prefix}-sg"
   }
+}
+
+data "aws_ami" "my_ami" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_key_pair" "ssh_key" {
+    key_name   = "myvm_ubuntu"
+    public_key = file(var.public_key_location)
+}
+
+resource "aws_instance" "myapp-server" {
+  ami           = data.aws_ami.my_ami.id
+  instance_type = var.instance_type
+  
+  availability_zone            = var.avail_zone
+  subnet_id                    = aws_subnet.my-vpc-sunbet-1.id
+  vpc_security_group_ids       = [aws_security_group.my-sg.id]
+  associate_public_ip_address  = true
+  key_name                     = aws_key_pair.ssh_key.key_name
+
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
+}
+
+output "image_id" {
+  value = data.aws_ami.my_ami.id
+}
+
+output "public_ip" {
+  value = aws_instance.myapp-server.public_ip
 }
